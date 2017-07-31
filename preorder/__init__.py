05c49847cc50435d3e768ced34aa3f9f2b120fc1
zeta17 <hendrik.zeta@gmail.com>
2017-07-30 18:49:23 -0700

    0.0.4

--- preorder/__init__.py
+++ preorder/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
+#0.0.4:
 #0.0.3:
 #   - Update Inquiry
 #0.0.2:
--- preorder/preorder/doctype/inquiry/inquiry.js
+++ preorder/preorder/doctype/inquiry/inquiry.js
@@ -3,13 +3,26 @@
 
 frappe.ui.form.on('Inquiry', {
 	refresh: function(frm) {
-
+		frm.set_df_property("inquiry_type", "read_only", frm.doc.__islocal ? 0 : 1);
 	}
 });
 cur_frm.set_query("contact_person",  function (frm) {
 		return {
         filters: [
-            ['customer', '=', cur_frm.doc.customerben]
+            ['customer', '=', cur_frm.doc.customer]
         ]
 		}
 });
+frappe.ui.form.on("Inquiry", "validate", function(frm) {
+	if(cur_frm.doc.inquiry_type == 'Request'){
+		cur_frm.doc.naming_series = 'R.YY.-.####'
+	}else if (cur_frm.doc.inquiry_type == 'Request Project') {
+		cur_frm.doc.naming_series = 'RP.YY.-.####'
+	}else if (cur_frm.doc.inquiry_type == 'Request Service') {
+		cur_frm.doc.naming_series = 'RS.YY.-.####'
+	}else if (cur_frm.doc.inquiry_type == 'Request BST') {
+		cur_frm.doc.naming_series = 'RBST.YY.-.####'
+	}else {
+		cur_frm.doc.naming_series = 'RPBST.YY.-.####'
+	}
+})
